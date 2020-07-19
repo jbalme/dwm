2ee4bbf818cad565cbd6c69253453eef6e1e7090
Jonathan Baldwin <jbaldwin9182@gmail.com>
2020-07-18 20:03:32 -0400

    add some more Windows-style binds, browser key

--- config.def.h
+++ config.def.h
@@ -64,6 +64,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *topcmd[]   = { "st", "-e", "htop", NULL };
+static const char *fmgrcmd[]  = { "nautilus", NULL };
+static const char *browsercmd = SHCMD("$BROWSER");
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -101,11 +104,15 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	{ Mod4Mask,                     XK_w,        spawn,          {.v = browsercmd} },
 	/* Windows-style binds */
 	{ Mod1Mask,                     XK_Tab,      focusstack,     {.i = +1 } },
 	{ Mod1Mask|ShiftMask,           XK_Tab,      focusstack,     {.i = -1 } },
 	{ Mod1Mask,                     XK_F2,	     spawn,          {.v = dmenucmd } },
 	{ Mod1Mask,                     XK_F4,       killclient,     {0}},
+	{ ControlMask|ShiftMask,        XK_Esc,	     spawn,		     {.v = topcmd} },
+	{ Mod4Mask,                     XK_f,        spawn,          {.v = fmgrcmd} },
 };
 
 /* button definitions */
