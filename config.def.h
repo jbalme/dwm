a69be4cd22b144034f437a9fee0514f513f163cf
Jonathan Baldwin <jbaldwin9182@gmail.com>
2020-08-19 06:30:01 -0400

    use envvar for terminal

--- config.def.h
+++ config.def.h
@@ -63,10 +63,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *topcmd[]   = { "st", "-e", "htop", NULL };
+static const char *termcmd[]  = { "/bin/sh", "-c", "exec $TERMINAL", NULL };
+static const char *topcmd[]   = { "/bin/sh", "-c", "exec $TERMINAL -e htop", NULL };
 static const char *fmgrcmd[]  = { "nautilus", NULL };
-static const char *webcmd[] = { "/bin/sh", "-c", "$BROWSER", NULL };
+static const char *webcmd[] = { "/bin/sh", "-c", "exec $BROWSER", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
